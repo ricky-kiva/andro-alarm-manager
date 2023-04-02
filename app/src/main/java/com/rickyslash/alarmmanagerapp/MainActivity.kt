dcf4a97d1e60e529cacadb6b4ae073c2f3fb217d
ricky-kiva <ricky.kiva@gmail.com>
2023-04-02 19:33:02 +0700

    add Main Actvt

--- app/src/main/java/com/rickyslash/alarmmanagerapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/alarmmanagerapp/MainActivity.kt
@@ -2,11 +2,88 @@ package com.rickyslash.alarmmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.rickyslash.alarmmanagerapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+// also extending interface 'DatePickerFragment.DialogDateListener' & 'TimePickerFragment.DialogTimeListener'
+class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
+
+    private var binding: ActivityMainBinding? = null
+    private lateinit var alarmReceiver: AlarmReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnOnceDate?.setOnClickListener(this)
+        binding?.btnOnceTime?.setOnClickListener(this)
+        binding?.btnSetOnceAlarm?.setOnClickListener(this)
+
+        alarmReceiver = AlarmReceiver()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_once_date -> {
+                val datePickerFragment = DatePickerFragment()
+                // showing the dialog
+                datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
+            }
+            R.id.btn_once_time -> {
+                val timePickerFragmentOne = TimePickerFragment()
+                // showing the dialog
+                timePickerFragmentOne.show(supportFragmentManager, TIME_PICKER_ONCE_TAG)
+            }
+            R.id.btn_set_once_alarm -> {
+                val onceDate = binding?.tvOnceDate?.text.toString()
+                val onceTime = binding?.tvOnceTime?.text.toString()
+                val onceMessage = binding?.edtOnceMessage?.text.toString()
+
+                // set the alarm with 'setOneTimeAlarm' method from 'AlarmReceiver'
+                alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME, onceDate, onceTime, onceMessage)
+            }
+        }
+    }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
+        // sets calendar to parameter from callback
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+        binding?.tvOnceDate?.text = dateFormat.format(calendar.time)
+    }
+
+    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
+        // sets calendar to parameter from callback
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        when (tag) {
+            TIME_PICKER_ONCE_TAG -> binding?.tvOnceTime?.text = dateFormat.format(calendar.time)
+            TIME_PICKER_REPEAT_TAG -> {}
+            else -> {
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
     }
 }
 
